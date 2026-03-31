af6fdffaef03dbd491db08ab8359514a33ab41c8
spendwise-admin <spendwise.admin@gmail.com>
2026-03-31 15:03:41 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,13 +1,11 @@
 const getBaseUrl = () => {
   const hostname = window.location.hostname;
 
-  // If running on localhost (laptop)
   if (hostname === "localhost" || hostname === "127.0.0.1") {
     return "http://localhost:4000";
   }
 
-  // If accessed from mobile or other device
-  return "https://spendwise-backend-1cf6.onrender.com";
+  return "https://spendwise-backend-e9bp.onrender.com";
 };
 
 export const API_BASE = getBaseUrl();
